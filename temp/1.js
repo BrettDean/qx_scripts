34300dc08b594d1b69c4129d241afa03b8fc3db0
Dean12321 <2333+Dean12321@noreply.github.com>
2021-04-02 22:25:36 +0800

    .

---
+++ temp/1.js
@@ -0,0 +1,108 @@
+{
+	"id": "hlyy",
+	"name": "葫芦音乐",
+	"keys": ["hlyyurl", "hlyyhd", "hlyyurl2", "hlyyhd2", "hlyyurl3", "hlyyhd3", "hlyyurl4", "hlyyhd4", "hlyyurl5", "hlyyhd5", "hlyyurl6", "hlyyhd6", "hlyyurl7", "hlyyhd7", "hlyyurl8", "hlyyhd8", "hlyyurl9", "hlyyhd9", "hlyyurl10", "hlyyhd10"],
+	"author": "@feizao",
+	"settings": [{
+		"id": "hlyystatus",
+		"name": "当前账号",
+		"val": "1",
+		"type": "number",
+		"desc": "当前账号对应"
+	},
+	{
+		"id": "hlyycount",
+		"name": "账号个数",
+		"val": "1",
+		"type": "number",
+		"desc": "需要几个账号，数字为几"
+	}],
+	"repo": "https://github.com/age174/-/blob/main/hlyy.js",
+	"icons": ["https://ae01.alicdn.com/kf/U49d941738c0c41569f7db55825943375f.jpg", "https://ae01.alicdn.com/kf/U49d941738c0c41569f7db55825943375f.jpg"],
+	"script": "https://raw.githubusercontent.com/age174/-/main/hlyy.js",
+	"icon": "https://ae01.alicdn.com/kf/U49d941738c0c41569f7db55825943375f.jpg",
+	"favIcon": "mdi-star",
+	"favIconColor": "primary",
+	"datas": [{
+		"key": "hlyyurl",
+		"val": "https://play.gxhuancai.com/hlplay/task/doTasks?appVersion=1.1.3&deviceId=BoMjlZYrtoRjTM0aippZtaaV5n4vt%2Bt3u1OTcY2V/ILLUhs302CNoOv7WoroL4Q%2BlbYxx9nYe/0JMWSoDYzIkoA%3D%3D&os=ios&tc=fyrEegQJ_uc8tcfU-pTXIQH0Hs6ltsads&tl=35&ut=eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI4MDY1NjE4NDMyMzI3MjIxMjQ4Iiwic3ViIjoiY2pXTFc1RU40S1FYTmFwcC1rVEk0WVBGemt3bG9oWndzIiwiaWF0IjoxNjE3MjM0MTYzLCJleHAiOjE2MjUwMTAxNjN9.jNPgSZq1lKpjynAaPGCk8FCFngB3Aek6ld4iLy6ML5g"
+	},
+	{
+		"key": "hlyyhd",
+		"val": "{\"Accept\":\"*/*\",\"Accept-Encoding\":\"br;q=1.0, gzip;q=0.9, deflate;q=0.8\",\"Connection\":\"keep-alive\",\"Host\":\"play.gxhuancai.com\",\"User-Agent\":\"hulumusic/1.1.3 (com.xiaoniu.hulumusic; build:22; iOS 14.4.0) Alamofire/5.4.1\",\"app_info\":\"1.1.3,ios,14.4,appstore,com.xiaoniu.hulumusic\",\"Accept-Language\":\"zh-Hans-CN;q=1.0, en-CN;q=0.9\",\"Content-Length\":\"0\"}"
+	},
+	{
+		"key": "hlyyurl2",
+		"val": ""
+	},
+	{
+		"key": "hlyyhd2",
+		"val": ""
+	},
+	{
+		"key": "hlyyurl3",
+		"val": ""
+	},
+	{
+		"key": "hlyyhd3",
+		"val": ""
+	},
+	{
+		"key": "hlyyurl4",
+		"val": ""
+	},
+	{
+		"key": "hlyyhd4",
+		"val": ""
+	},
+	{
+		"key": "hlyyurl5",
+		"val": ""
+	},
+	{
+		"key": "hlyyhd5",
+		"val": ""
+	},
+	{
+		"key": "hlyyurl6",
+		"val": ""
+	},
+	{
+		"key": "hlyyhd6",
+		"val": ""
+	},
+	{
+		"key": "hlyyurl7",
+		"val": ""
+	},
+	{
+		"key": "hlyyhd7",
+		"val": ""
+	},
+	{
+		"key": "hlyyurl8",
+		"val": ""
+	},
+	{
+		"key": "hlyyhd8",
+		"val": ""
+	},
+	{
+		"key": "hlyyurl9",
+		"val": ""
+	},
+	{
+		"key": "hlyyhd9",
+		"val": ""
+	},
+	{
+		"key": "hlyyurl10",
+		"val": ""
+	},
+	{
+		"key": "hlyyhd10",
+		"val": ""
+	}],
+	"sessions": [],
+	"isLoaded": true
+}
