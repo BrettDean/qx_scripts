54e5f9f3f9306edd8baa45837dc65771a69ccd70
Dean12321 <2333+Dean12321@noreply.github.com>
2021-04-03 22:26:20 +0800

    .

--- temp/all.js
+++ temp/all.js
@@ -25,19 +25,19 @@
 	"favIconColor": "primary",
 	"datas": [{
 		"key": "ximeiurl",
-		"val": "https://app.hubonews.com/v4/articles/list"
-	},
-	{
-		"key": "ximeihd",
-		"val": "{\"Accept-Encoding\":\"gzip, deflate, br\",\"Host\":\"app.hubonews.com\",\"X_CLIENT_CHANNEL\":\"App Store\",\"X_CLIENT_APP\":\"com.tigerye.tigerobo\",\"X_CLIENT_PLATFORM\":\"iOS\",\"Connection\":\"keep-alive\",\"DEVICE_ID\":\"ED0A035F-3074-419A-AC01-C244DF2C51B7\",\"BRAND_PRODUCT\":\"Apple-iPhone13,2\",\"X_CLIENT_TRANSLATION_LANGUAGE\":\"en\",\"Accept-Language\":\"zh-Hans-CN;q=1, en-CN;q=0.9\",\"User-Agent\":\"tigerobo/2.6.4 (iPhone; iOS 14.4; Scale/3.00)\",\"Content-Type\":\"application/json\",\"X_CLIENT_IDFA\":\"D133DCB7-679B-454F-8466-1FA2A6DB6738\",\"X_CLIENT_COUNTRY\":\"CN\",\"X_CLIENT_VERSION\":\"2.6.4\",\"Accept\":\"*/*\",\"API_AUTHORIZATION\":\"vHuDxnCSIhaqSvQKRgIK\",\"X_CLIENT_BUILD\":\"220\",\"X_CLIENT_LANGUAGE\":\"zh\",\"Content-Length\":\"21\"}"
-	},
-	{
-		"key": "ximeiurl2",
 		"val": "https://app.hubonews.com/v2/articles/list"
 	},
 	{
-		"key": "ximeihd2",
+		"key": "ximeihd",
 		"val": "{\"Accept-Encoding\":\"gzip, deflate, br\",\"Host\":\"app.hubonews.com\",\"X_CLIENT_CHANNEL\":\"App Store\",\"X_CLIENT_APP\":\"com.tigerye.tigerobo\",\"X_CLIENT_PLATFORM\":\"iOS\",\"Connection\":\"keep-alive\",\"DEVICE_ID\":\"ED0A035F-3074-419A-AC01-C244DF2C51B7\",\"BRAND_PRODUCT\":\"Apple-iPhone13,2\",\"X_CLIENT_TRANSLATION_LANGUAGE\":\"en\",\"Accept-Language\":\"zh-Hans-CN;q=1, en-CN;q=0.9\",\"User-Agent\":\"tigerobo/2.6.4 (iPhone; iOS 14.4; Scale/3.00)\",\"Content-Type\":\"application/json\",\"X_CLIENT_IDFA\":\"D133DCB7-679B-454F-8466-1FA2A6DB6738\",\"X_CLIENT_COUNTRY\":\"CN\",\"X_CLIENT_VERSION\":\"2.6.4\",\"Accept\":\"*/*\",\"API_AUTHORIZATION\":\"sFGAGILUVoqENPVcjIUx\",\"X_CLIENT_BUILD\":\"220\",\"X_CLIENT_LANGUAGE\":\"zh\",\"Content-Length\":\"55\"}"
+	},
+	{
+		"key": "ximeiurl2",
+		"val": "https://app.hubonews.com/v4/articles/list"
+	},
+	{
+		"key": "ximeihd2",
+		"val": "{\"Accept-Encoding\":\"gzip, deflate, br\",\"Host\":\"app.hubonews.com\",\"X_CLIENT_CHANNEL\":\"App Store\",\"X_CLIENT_APP\":\"com.tigerye.tigerobo\",\"X_CLIENT_PLATFORM\":\"iOS\",\"Connection\":\"keep-alive\",\"DEVICE_ID\":\"ED0A035F-3074-419A-AC01-C244DF2C51B7\",\"BRAND_PRODUCT\":\"Apple-iPhone13,2\",\"X_CLIENT_TRANSLATION_LANGUAGE\":\"en\",\"Accept-Language\":\"zh-Hans-CN;q=1, en-CN;q=0.9\",\"User-Agent\":\"tigerobo/2.6.4 (iPhone; iOS 14.4; Scale/3.00)\",\"Content-Type\":\"application/json\",\"X_CLIENT_IDFA\":\"D133DCB7-679B-454F-8466-1FA2A6DB6738\",\"X_CLIENT_COUNTRY\":\"CN\",\"X_CLIENT_VERSION\":\"2.6.4\",\"Accept\":\"*/*\",\"API_AUTHORIZATION\":\"NceuoaZthvjperNrGOND\",\"X_CLIENT_BUILD\":\"220\",\"X_CLIENT_LANGUAGE\":\"zh\",\"Content-Length\":\"21\"}"
 	},
 	{
 		"key": "ximeiurl3",
@@ -45,7 +45,7 @@
 	},
 	{
 		"key": "ximeihd3",
-		"val": "{\"Accept-Encoding\":\"gzip, deflate, br\",\"Host\":\"app.hubonews.com\",\"X_CLIENT_CHANNEL\":\"App Store\",\"X_CLIENT_APP\":\"com.tigerye.tigerobo\",\"X_CLIENT_PLATFORM\":\"iOS\",\"Connection\":\"keep-alive\",\"DEVICE_ID\":\"ED0A035F-3074-419A-AC01-C244DF2C51B7\",\"BRAND_PRODUCT\":\"Apple-iPhone13,2\",\"X_CLIENT_TRANSLATION_LANGUAGE\":\"en\",\"Accept-Language\":\"zh-Hans-CN;q=1, en-CN;q=0.9\",\"User-Agent\":\"tigerobo/2.6.4 (iPhone; iOS 14.4; Scale/3.00)\",\"Content-Type\":\"application/json\",\"X_CLIENT_IDFA\":\"D133DCB7-679B-454F-8466-1FA2A6DB6738\",\"X_CLIENT_COUNTRY\":\"CN\",\"X_CLIENT_VERSION\":\"2.6.4\",\"Accept\":\"*/*\",\"API_AUTHORIZATION\":\"NceuoaZthvjperNrGOND\",\"X_CLIENT_BUILD\":\"220\",\"X_CLIENT_LANGUAGE\":\"zh\",\"Content-Length\":\"21\"}"
+		"val": "{\"Accept-Encoding\":\"gzip, deflate, br\",\"Host\":\"app.hubonews.com\",\"X_CLIENT_CHANNEL\":\"App Store\",\"X_CLIENT_APP\":\"com.tigerye.tigerobo\",\"X_CLIENT_PLATFORM\":\"iOS\",\"Connection\":\"keep-alive\",\"DEVICE_ID\":\"ED0A035F-3074-419A-AC01-C244DF2C51B7\",\"BRAND_PRODUCT\":\"Apple-iPhone13,2\",\"X_CLIENT_TRANSLATION_LANGUAGE\":\"en\",\"Accept-Language\":\"zh-Hans-CN;q=1, en-CN;q=0.9\",\"User-Agent\":\"tigerobo/2.6.4 (iPhone; iOS 14.4; Scale/3.00)\",\"Content-Type\":\"application/json\",\"X_CLIENT_IDFA\":\"D133DCB7-679B-454F-8466-1FA2A6DB6738\",\"X_CLIENT_COUNTRY\":\"CN\",\"X_CLIENT_VERSION\":\"2.6.4\",\"Accept\":\"*/*\",\"API_AUTHORIZATION\":\"vHuDxnCSIhaqSvQKRgIK\",\"X_CLIENT_BUILD\":\"220\",\"X_CLIENT_LANGUAGE\":\"zh\",\"Content-Length\":\"21\"}"
 	},
 	{
 		"key": "ximeiurl4",
